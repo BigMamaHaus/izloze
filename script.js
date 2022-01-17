42a504bc166922c91f59c6931b764531503020d3
BigMamaHaus <97232612+BigMamaHaus@users.noreply.github.com>
2022-01-17 15:08:02 +0200

    tresais

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const vardi=['Maigonis Maigulis','Stīvs Gailis','Eina Huia','Valdis Kulis','Hen
 const Balvas=['Spa masāža','Ābols','Dzēriens','Lamborgini','NFT'];
 const naudaKopa=1000000;//kopeja summa
 let uzvaretajuSkaits=5;
-let rand= Math.random()*10;
+let rand= Math.random()*5;
 rand= Math.floor(rand);//noapaļo uz leju
 console.log(vardi[rand]);//izvada konsolē
