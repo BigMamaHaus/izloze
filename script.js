b67285251e65a9023c5d1e589eff0b93ca0f7e07
BigMamaHaus <97232612+BigMamaHaus@users.noreply.github.com>
2022-01-24 15:04:01 +0200

    otra stunda

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const vardi=['Maigonis Maigulis','Stīvs Gailis','Eina Huia','Valdis Kulis','Henrijs Suns'];
+const vardi=['Maigonis Maigulis','Stivs Gailis','Eina Huia','Valdis Kulis','Henrijs Suns'];
 const Balvas=['Spa masāža','Ābols','Dzēriens','Lamborgini','NFT'];
 const naudaKopa=1000000;//kopeja summa
 let uzvaretajuSkaits=5;
-let rand= Math.random()*5;
+let rindas= document.querySelector(`.rindas`);
+rindas.innerHTML=` `;
+for(let i=0;i<5;i++){
+let rand= Math.random()*vardi.length;
 rand= Math.floor(rand);//noapaļo uz leju
 console.log(vardi[rand]);//izvada konsolē
+}
+rindas.innerHTML=
+<tr>
+    <td>i</td>
+    <td>Uzvarētājs</td>
+</tr>
