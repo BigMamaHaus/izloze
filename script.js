d8f20179e44b67ea41a9c75afc611988431f9a60
BigMamaHaus <97232612+BigMamaHaus@users.noreply.github.com>
2022-01-31 14:40:05 +0200

    izloze frl

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const vardi=['Maigonis Maigulis','Stivs Gailis','Eina Huia','Valdis Kulis','Henrijs Suns'];
-const Balvas=['Spa masāža','Ābols','Dzēriens','Lamborgini','NFT'];
-const naudaKopa=1000000;//kopeja summa
-let uzvaretajuSkaits=5;
-let rindas= document.querySelector(`.rindas`);
-rindas.innerHTML=` `;
-for(let i=0;i<5;i++){
-let rand= Math.random()*vardi.length;
-rand= Math.floor(rand);//noapaļo uz leju
-console.log(vardi[rand]);//izvada konsolē
+const vardi = ['Maigonis Maigulis', 'Stivs Gailis', 'Eina Huia', 'Valdis Kulis', 'Henrijs Suns'];
+const Balvas = ['Spa masāža', 'Ābols', 'Dzēriens', 'Lamborgini', 'NFT'];
+const naudaKopa = 1000000;//kopeja summa
+let uzvaretajuSkaits = 5;
+let rindas = document.querySelector(`.rindas`);
+rindas.innerHTML = ` `;
+for (let i = 0; i < 5; i++) {
+    let rand = Math.random() * vardi.length;
+    rand = Math.floor(rand);//noapaļo uz leju
+    console.log(vardi[rand]);//izvada konsolē
 }
-rindas.innerHTML=
-<tr>
-    <td>i</td>
-    <td>Uzvarētājs</td>
-</tr>
+rindas.innerHTML =
+    <tr>
+        <td>i</td>
+        <td>Uzvarētājs</td>
+    </tr>
